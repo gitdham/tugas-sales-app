278e9564ca3bbd6c0fff009ad083e94ec92e576d
gitdham <adzani234@gmail.com>
2024-11-16 16:34:36 +0700

    chore: create idr js formatter

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import idrFormat from './idrFormat';
 window.axios = axios;
+window.idrFormat = idrFormat;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
---
+++ resources/js/idrFormat.js
@@ -0,0 +1,3 @@
+export default function idrFormat(number) {
+  return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(number)
+}
